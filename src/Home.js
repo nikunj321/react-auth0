983b50d2aee6dd2876721cf246d785e7b21635f1
nikunj321 <nikunjc43@gmail.com>
2021-01-08 08:51:20 +0530

    adding existing file

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
+class Home extends Component {
+    render() {
+        const { login, isAuthenticated } = this.props.auth;
+        return (
+            <>
+                <h1>Home</h1>
+                {isAuthenticated() ? <Link to="/profile">View Profile</Link> : (<button onClick={login} >Log In</button>)}                
+            </>
+        )
+    }
+}
+
+export default Home
